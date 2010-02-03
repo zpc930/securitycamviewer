b05bbc94cecf8b2c42c3724c43e35983b16778d6
josiahbryan <josiahbryan@a10b5d5e-10d8-11df-a8dc-870042ab4e57>
2010-02-03 19:54:44 +0000

    Successfully tested muxer via SSH tunnel over 120 kbps wireless uplink from remote site with 4x 640x480 cameras.

--- src/muxer/main.cpp
+++ src/muxer/main.cpp
@@ -7,15 +7,15 @@
 
 int main(int argc, char *argv[])
 {
-// 	pid_t pid;
-// 	pid = fork();
-// 	if(pid == 0)
-// 	{
+ 	pid_t pid;
+ 	pid = fork();
+ 	if(pid == 0)
+ 	{
 		QCoreApplication app(argc, argv);
 		Muxer mux;
 		return app.exec();
-/*		_exit(app.exec());
+		_exit(app.exec());
 	}
 	printf("Forked muxer with pid %d\n", (int)pid);
-	return 0;*/
+	return 0;
 }
