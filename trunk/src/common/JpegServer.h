667961926044f43a212247c577c2f718171b1d04
josiahbryan <josiahbryan@a10b5d5e-10d8-11df-a8dc-870042ab4e57>
2010-02-03 19:31:16 +0000

    Added JpegServer.cpp|h missing from r5

---
+++ trunk/src/common/JpegServer.h
@@ -0,0 +1,58 @@
+#ifndef JpegServer_H
+#define JpegServer_H
+
+#include <QTcpServer>
+#include <QThread>
+#include <QTcpSocket>
+
+class JpegServer : public QTcpServer
+{
+	Q_OBJECT
+	
+public:
+	JpegServer(QObject *parent = 0);
+	
+	void setProvider(QObject *provider, const char * signalName);
+	
+// 	QString myAddress();
+
+protected:
+	void incomingConnection(int socketDescriptor);
+
+private:
+	QObject * m_imageProvider;
+	const char * m_signalName;
+
+};
+
+
+class QImage;
+class JpegServerThread : public QThread
+{
+    Q_OBJECT
+
+public:
+	JpegServerThread(int socketDescriptor, QObject *parent = 0);
+	~JpegServerThread();
+	
+	void run();
+
+signals:
+	void error(QTcpSocket::SocketError socketError);
+
+public slots:
+	void imageReady(QImage*);
+
+private:
+	void writeHeaders();
+	
+	int m_socketDescriptor;
+	QTcpSocket * m_socket;
+	
+	QByteArray m_boundary;
+	
+};
+
+
+#endif //JpegServer_H
+
