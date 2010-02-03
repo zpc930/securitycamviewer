facc6376c6ebc19c8d3d93faa40a0bf95d0a0dd1
josiahbryan <josiahbryan@a10b5d5e-10d8-11df-a8dc-870042ab4e57>
2010-02-03 22:11:21 +0000

    Added getopt support to client for use in the '-c' command line argument. Somehow, SecurityCamViewer.pro was missing from /src/client - adding it here.

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,11 +1,37 @@
 #include <QApplication>
 #include "MainWindow.h"
 
+#include "../common/getopt/getopt.h"
+
+
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
-    MainWindow viewer;
-    viewer.show();
+	QApplication app(argc, argv);
+	
+	// construct class from command line arguments
+	GetOpt opts(argc, argv);
+	
+	bool verbose = false;
+	opts.addSwitch("verbose", &verbose);
 
-    return app.exec();
+	// add some switches
+	QString configFile;
+	opts.addOptionalOption('c',"config", &configFile, "viewer.ini");
+	
+	// do the parsing and check for errors
+	if (!opts.parse()) 
+	{
+		fprintf(stderr,"Usage: %s [--verbose] [-c|--config configfile]\n", qPrintable(opts.appName()));
+		return 1;
+	}
+	
+	if(configFile.isEmpty())
+		configFile = "viewer.ini";
+			
+		
+		
+	MainWindow viewer(configFile,verbose);
+	viewer.show();
+	
+	return app.exec();
 }
