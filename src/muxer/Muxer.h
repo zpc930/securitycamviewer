12bd3a165b0e0eaf2f929d8ffbb69ab77869b156
josiahbryan <josiahbryan@a10b5d5e-10d8-11df-a8dc-870042ab4e57>
2010-02-03 19:28:36 +0000

    Added prototype muxer

---
+++ src/muxer/Muxer.h
@@ -0,0 +1,48 @@
+#ifndef Muxer_H
+#define Muxer_H
+
+#include <QObject>
+#include <QTimer>
+#include <QImage>
+#include <QSize>
+
+#define LISTEN_PORT 8088
+
+class JpegServer;
+class MjpegClient;
+
+class Muxer : public QObject
+{
+	Q_OBJECT
+public:
+	Muxer(QObject *parent =0);
+	~Muxer();
+
+signals:
+	void imageReady(QImage*);
+
+private slots:
+	void newImage(QImage);
+	void updateFrames();
+	
+
+private:
+	void applySize(int x, int y);
+	
+	JpegServer * m_jpegServer;
+	
+	QList<MjpegClient*> m_threads;
+	QList<QImage> m_images;
+	QTimer m_updateTimer;
+	
+	QImage m_muxedImage;
+	
+	int m_cols;
+	int m_rows;
+	QSize m_frameSize;
+	
+	QList<bool> m_wasChanged;
+
+};
+
+#endif // Muxer_H
